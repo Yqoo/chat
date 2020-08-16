422f0b2fe4d6b8afa57e3dcbdc9c972148b58245
GARNiDELiAYoo“git config --global user.name GARNiDELiAYoo“ <garnideliayoo@163.comgit config --global user.email garnideliayoo@163.com>
2020-08-16 21:31:35 +0800

    chat

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -2,11 +2,25 @@
   <div class="comp-chat">
     <p class="title"><i class="el-icon-s-promotion"></i>聊天</p>
     <div class="chatBox">
-      <div class="header">
-        某某用户
-      </div>
-      <div class="body"></div>
-      <textarea class="footer"></textarea>
+      <el-row>
+        <el-col :span="6">
+          <div class="header" style="border-top-left-radius: 10px;">
+            聊天列表
+          </div>
+          <ul class="userList">
+            <li v-for="list in 100" :key="list" @click="setUser(list)">
+              用户{{ list }}
+            </li>
+          </ul>
+        </el-col>
+        <el-col :span="18">
+          <div class="header" style="border-top-right-radius: 10px;">
+            用户{{ user }}
+          </div>
+          <div class="body"></div>
+          <textarea class="footer"></textarea>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
@@ -15,7 +29,14 @@
 export default {
   name: "Chat",
   data() {
-    return {};
+    return {
+      user: "xxx"
+    };
+  },
+  methods: {
+    setUser(user) {
+      this.user = user;
+    }
   }
 };
 </script>
@@ -38,15 +59,44 @@ export default {
     left: 50%;
     transform: translate(-50%, -50%);
     height: 70vh;
-    width: 30vw;
+    width: 50vw;
     border: 1px solid #ddd;
     border-radius: 10px;
     box-shadow: 0px 0px 15px #ddd;
+    .el-row {
+      height: 100%;
+      width: 100%;
+      .el-col {
+        height: 100%;
+      }
+    }
+    .userList {
+      height: calc(100% - 42px);
+      overflow-y: auto;
+      overflow-x: hidden;
+      width: 100%;
+      border: 1px solid #ddd;
+      li {
+        height: 40px;
+        width: 100%;
+        line-height: 40px;
+        padding-left: 10px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        cursor: pointer;
+      }
+    }
+    .userList::-webkit-scrollbar {
+      display: none;
+    }
     .header {
       height: 40px;
       line-height: 40px;
       text-align: center;
       border-bottom: 1px solid #ddd;
+      background: #409eff;
+      color: #fff;
     }
     .body {
       height: calc(70vh - 170px);
